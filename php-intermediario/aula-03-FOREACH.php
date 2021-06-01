a460f9a38f706b8695675db86ab95398a86095d2
Yuri <lacerda119yuri@gmail.com>
2021-05-31 23:15:26 -0300

    quinto commit de estudo

--- php-intermediario/aula-03-FOREACH.php
+++ php-intermediario/aula-03-FOREACH.php
@@ -11,25 +11,23 @@ foreach($aluno as $chave => $dados)
     echo $chave." = ".$dados."<br/>";
 }
 
-?>
-<?php
-//$name = array(
-//    array("nome" => "Yuri", "idade" => 23),
-//    array("nome" => "Vinicius", "idade" => 25),
-//    array("nome" => "Fulano", "idade" => 24),
-//    array("nome" => "Beltrano", "idade" => 20)
-//);
-//foreach($name as $aluno)
-//    {
-//        echo "Aluno: ".$aluno["nome"]." - Idade: ".$aluno["idade"]."<br/>";
-//    }
-//
+$name = array(
+   array("nome" => "Yuri", "idade" => 23),
+   array("nome" => "Vinicius", "idade" => 25),
+   array("nome" => "Fulano", "idade" => 24),
+   array("nome" => "Beltrano", "idade" => 20)
+);
+foreach($name as $aluno)
+   {
+       echo "Aluno: ".$aluno["nome"]." - Idade: ".$aluno["idade"]."<br/>";
+   }
+
 //ele funciona auxiliando o processo de listagem de ARRAY's
-//
-//$nomes = array("Yuri", "Vinicius", "Fulano", "Beltrano");
-//    foreach($nomes as $aluno)
-//    {
-//        echo "Aluno: ".$aluno."<br/>";
-//    }
+
+$nomes = array("Yuri", "Vinicius", "Fulano", "Beltrano");
+   foreach($nomes as $aluno)
+   {
+       echo "Aluno: ".$aluno."<br/>";
+   }
 ?>
 
---
+++ php-intermediario/aula-07-manipulacao-de-textos.php
@@ -0,0 +1,33 @@
+<?php
+    $nome = "Yuri Lacerda";
+        $x = explode(" ", $nome);
+
+    print_r($x)."<br/>"; //função separar string, formando outras strings.
+    
+
+    $array = array("Yuri", "Lacerda");
+        $nomecompleto = implode(" ", $array);
+    echo $nomecompleto."<br/>"; //implode tem a função de juntar um array.
+    
+
+    $X = number_format(8.2578419, 2);
+    echo $X."<br/>"; //Formata numeros de diversas fomras possiveis
+    
+    $texto = "minha cachorra mordeu meu chinelo!";
+    $string = str_replace("mordeu", "destruiu", $texto);
+        echo $string."<br/>"; // função de trocar palavras de uma string.
+    
+    echo strtolower("YURI GUIMARAES")."<br/>";//transformar tudo que está maiusculo em minusculo
+    echo strtoupper("yuri lacerda")."<br/>"; //transformar tudo que  esta minusculo em maisculo
+
+    $substr = "YURI LACERDA";
+    $xx = substr($substr, 0, 4);
+        echo $xx."<br/>"; // Função é voce pegar parte de uma string
+
+    $ucfirst = "laila";
+        echo ucfirst($ucfirst)."<br/>"; //tem função de transformar a primeira letra da primeira palavra de uma string em Maiusculo
+
+    $ucwords = "laila gabriely sampaio lima";
+        echo ucwords($ucwords)."<br/>";// tem função de modificar todas as palavras colocando todas as primeiras letras em maiusculo
+     
+?>
